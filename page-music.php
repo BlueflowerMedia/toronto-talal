05b9f56900b5673a2c0aca7c68a25d35b0a375df
bnodthapa@gmail.com <bthapa7@senecacollege.ca>
2018-07-19 15:13:54 -0400

    first attempt with embedded code

--- page-music.php
+++ page-music.php
@@ -6,6 +6,7 @@
        <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.2.0/anime.js"></script>
     </div>
   <div class="container-track">
+    <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/441489744&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
     <?php
     if(have_posts()){
         while(have_posts()){
