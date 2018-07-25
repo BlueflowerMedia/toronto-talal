a26ee6f2de880474c8c4408451d3a1835781c28e
bnodthapa@gmail.com <bthapa7@senecacollege.ca>
2018-07-25 14:37:32 -0400

    front page completed

--- front-page.php
+++ front-page.php
@@ -1,17 +1,22 @@
 <?php get_header(); ?>
+
   <!-- Section - Main Container -->
   <div class="main-container">
+
     <!-- Section - Background Video -->
     <div id="myVideo">
       <video autoplay muted loop >
         <source src="<?php echo get_template_directory_uri(); ?>/videos.mp4" type="video/mp4">
       </video>
     </div>
+
     <!-- Section - Content -->
     <div class="container-content">
       <div class="talal">
         <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-          <img id="pic-logo" src="<?php echo get_template_directory_uri(); ?>/Images/talal-logo.svg" alt="">
+          <!-- <img id="pic-logo" src="<?php echo get_template_directory_uri(); ?>/Images/talal-logo.svg" alt=""> -->
+          <?php echo file_get_contents(get_template_directory_uri()."/images/talal-logo.svg") ?>
+
         </a>
         <div class="para">
           <a href="<?php echo get_permalink(get_page_by_path( 'music' )); ?>">Music</a>
